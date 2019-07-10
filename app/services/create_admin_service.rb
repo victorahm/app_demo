c720650327e7bfe2aa3b3e00bc360b66b59643b1
Victor Herrera <victorahm@gmail.com>
2019-07-10 00:41:08 -0500

    Fix seed file
    :100644 100644 c0bcb75 c5676f8 M	app/services/create_admin_service.rb

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -1,10 +1,11 @@
 class CreateAdminService
   def call
-    user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
+    role = Role.find_or_create_by!(name: Rails.application.secrets.admin_role)
+
+    user = role.users.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
         user.password = Rails.application.secrets.admin_password
         user.password_confirmation = Rails.application.secrets.admin_password
-        user.name= "Default User"
-        user.admin!
+        user.name= Rails.application.secrets.admin_name
       end
   end
 end
